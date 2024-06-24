ac20e73ad9d5c2b3e477346e1d592dab3f954ade
Umair Karel <umair.karel@sarvaha.com>
2024-06-24 10:31:55 +0530

    refactor #1: refactor code to fetch separate reports

--- backup.py
+++ backup.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from google.oauth2 import service_account
@@ -11,7 +12,7 @@ GA_API_KEYS = "config/api-keys.json"
 """### Define Procedures"""
 
 
-def create_body(body, view_id, page_size=10000, page_token="0"):
+def create_body(body, view_id, page_size=100000, page_token="0"):
     body["reportRequests"][0]["viewId"] = view_id
     body["reportRequests"][0]["pageSize"] = page_size
     body["reportRequests"][0]["pageToken"] = page_token
@@ -122,7 +123,7 @@ def format_report(response):
     return pd.concat([summary, pivot], axis=1)
 
 
-def run_report(body, view_id, credentials_file, page_size=10000):
+def run_report(body, view_id, credentials_file, page_size=100000):
     # Create service credentials
     credentials = service_account.Credentials.from_service_account_file(
         credentials_file, scopes=["https://www.googleapis.com/auth/analytics.readonly"]
@@ -137,6 +138,8 @@ def run_report(body, view_id, credentials_file, page_size=10000):
         .batchGet(body=create_body(body, view_id, page_size, page_token))
         .execute()
     )
+    print("Data fetch complete!\nCreating Dataframe")
+
     df = format_report(response)
 
     # If the response has nextPageToken, continue
@@ -148,6 +151,8 @@ def run_report(body, view_id, credentials_file, page_size=10000):
             .execute()
         )
         df = pd.concat([df, format_report(response)])
+
+    print("Dataframe creation complete!")
 
     return df
 
@@ -171,75 +176,249 @@ if __name__ == "__main__":
     # Constants
     VIEWS = read_json_to_dict("./config/views.json")
     PERIOD = "yearMonth"
-    REPORT_CODE = "Acquisition"
 
     # Reporting Variables
-    END_DATE = "2023-06-20"
+    END_DATE = datetime.today().strftime("%Y-%m-%d")
 
     # Metrics
-    output_metrics = [
-        {"expression": "ga:sessions"},
-        {"expression": "ga:users"},
-        {"expression": "ga:newUsers"},
-        {"expression": "ga:bounces"},
-        {"expression": "ga:bounceRate"},
-        {"expression": "ga:avgSessionDuration"},
-        {"expression": "ga:pageviews"},
-        {"expression": "ga:uniquePageviews"},
-        {"expression": "ga:pageviewsPerSession"},
-    ]
-
-    DIMS = {
+    output_metrics = {
         "Audience": [
-            {"name": "ga:language"},
-            {"name": "ga:country"},
-            {"name": "ga:city"},
-            {"name": "ga:deviceCategory"},
-            {"name": "ga:mobileDeviceInfo"},
-            {"name": "ga:browser"},
-            {"name": "ga:operatingSystem"},
-            {"name": "ga:networkLocation"},
-            # {"name": "ga:userAgeBracket"},
-            # {"name": "ga:userGender"},
+            {"expression": "ga:users"},
+            {"expression": "ga:newUsers"},
+            {"expression": "ga:sessions"},
+            {"expression": "ga:bounceRate"},
+            {"expression": "ga:pageviewsPerSession"},
+            {"expression": "ga:avgSessionDuration"},
+            {"expression": "ga:transactions"},
+            {"expression": "ga:transactionRevenue"},
+            {"expression": "ga:transactionsPerSession"},
+            {"expression": "ga:revenuePerTransaction"},
         ],
-        "Acquisition": [
-            {"name": "ga:source"},
-            {"name": "ga:medium"},
-            {"name": "ga:campaign"},
-            {"name": "ga:sourceMedium"},
-            {"name": "ga:referralPath"},
-            {"name": "ga:channelGrouping"},
-            # {"name": ""},
+        "Audience_Behavior": [
+            {"expression": "ga:sessions"},
+            {"expression": "ga:pageviews"},
+        ],
+        "Behavior": [
+            {"expression": "ga:pageviews"},
+            {"expression": "ga:uniquePageviews"},
+            {"expression": "ga:timeOnPage"},
+            {"expression": "ga:entrances"},
+            {"expression": "ga:bounceRate"},
+            {"expression": "ga:exits"},
+            {"expression": "ga:pageValue"},
+        ],
+        "Exits": [
+            {"expression": "ga:exits"},
+            {"expression": "ga:pageviews"},
+        ],
+        "Events": [
+            {"expression": "ga:totalEvents"},
+            {"expression": "ga:uniqueEvents"},
+            {"expression": "ga:eventValue"},
+            {"expression": "ga:avgEventValue"},
+        ],
+        "Goals": [
+            {"expression": "ga:goalCompletionsAll"},
+            {"expression": "ga:goalValueAll"},
+        ],
+        "Ecomm_Product": [
+            {"expression": "ga:itemRevenue"},
+            {"expression": "ga:uniquePurchases"},
+            {"expression": "ga:itemQuantity"},
+            {"expression": "ga:revenuePerItem"},
+            {"expression": "ga:itemsPerPurchase"},
+            {"expression": "ga:productRefunds"},
+        ],
+        "Ecomm_Sales": [
+            {"expression": "ga:transactionRevenue"},
+            {"expression": "ga:transactionTax"},
+            {"expression": "ga:transactionShipping"},
+            {"expression": "ga:refundAmount"},
+            {"expression": "ga:itemQuantity"},
         ],
     }
 
-    # Dimensions
-    output_dimensions = [{"name": "ga:yearMonth"}, *DIMS[REPORT_CODE]]
+    Reports = {
+        "Audience": {
+            "Geo_Language": {
+                "Dims": [
+                    {"name": "ga:language"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+            "Behavior_NewVsReturning": {
+                "Dims": [
+                    {"name": "ga:userType"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+            "Behavior_Frequency": {
+                "Dims": [
+                    {"name": "ga:sessionCount"},
+                    {"name": "ga:daysSinceLastSession"},
+                ],
+                "metrics": output_metrics["Audience_Behavior"],
+            },
+            "Behavior_Engagement": {
+                "Dims": [
+                    {"name": "ga:sessionDurationBucket"},
+                    {"name": "ga:pageDepth"},
+                ],
+                "metrics": output_metrics["Audience_Behavior"],
+            },
+            "Technology_Browser_OS": {
+                "Dims": [
+                    {"name": "ga:browser"},
+                    {"name": "ga:operatingSystem"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+            # needs change
+            "Technology_Network": {
+                "Dims": [
+                    {"name": "ga:networkLocation"},
+                    # {"name": "ga:hostname"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+            "Mobile": {
+                "Dims": [{"name": "ga:mobileDeviceInfo"}],
+                "metrics": output_metrics["Audience"],
+            },
+            "Geo_Location": {
+                "Dims": [
+                    {"name": "ga:continent"},
+                    # {"name": "ga:subContinent"},
+                    {"name": "ga:country"},
+                    {"name": "ga:city"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+        },
+        "Acquisition": {
+            "Channels": {
+                "Dims": [
+                    {"name": "ga:source"},
+                    {"name": "ga:medium"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+            "Treemaps": {
+                "Dims": [
+                    {"name": "ga:channelGrouping"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+            "Campaigns": {
+                "Dims": [
+                    {"name": "ga:campaign"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+        },
+        "Behavior": {
+            "All_Pages": {
+                "Dims": [
+                    {"name": "ga:pagePath"},
+                    {"name": "ga:pageTitle"},
+                ],
+                "metrics": output_metrics["Behavior"],
+            },
+            "Landing_Pages": {
+                "Dims": [
+                    {"name": "ga:landingPagePath"},
+                    {"name": "ga:pageTitle"},
+                ],
+                "metrics": output_metrics["Audience"],
+            },
+            "Exit_Pages": {
+                "Dims": [
+                    {"name": "ga:exitPagePath"},
+                    {"name": "ga:pageTitle"},
+                ],
+                "metrics": output_metrics["Exits"],
+            },
+            "Top_Events": {
+                "Dims": [
+                    {"name": "ga:eventCategory"},
+                    {"name": "ga:eventAction"},
+                    {"name": "ga:eventLabel"},
+                ],
+                "metrics": output_metrics["Events"],
+            },
+            "Events_Pages": {
+                "Dims": [
+                    {"name": "ga:exitPagePath"},
+                    {"name": "ga:pageTitle"},
+                ],
+                "metrics": output_metrics["Events"],
+            },
+        },
+        "Conversions": {
+            "Goals": {
+                "Dims": [
+                    {"name": "ga:goalCompletionLocation"},
+                ],
+                "metrics": output_metrics["Goals"],
+            },
+            "Ecomm_Product_Performance": {
+                "Dims": [
+                    {"name": "ga:productName"},
+                    {"name": "ga:productBrand"},
+                    {"name": "ga:productSku"},
+                ],
+                "metrics": output_metrics["Ecomm_Product"],
+            },
+            "Ecomm_Sales_Performance": {
+                "Dims": [
+                    {"name": "ga:transactionId"},
+                ],
+                "metrics": output_metrics["Ecomm_Sales"],
+            },
+        },
+    }
 
     # Run Your Report and Save
     for view_id, view_details in VIEWS.items():
-        # Construct your Request
-        report_request = {
-            "reportRequests": [
-                {
-                    "dateRanges": [
-                        {"startDate": view_details["startDate"], "endDate": END_DATE}
-                    ],
-                    "metrics": output_metrics,
-                    "dimensions": output_dimensions,
+        for report in Reports:
+            subReports = Reports[report]
+            for sub_report in subReports:
+                subReport = subReports[sub_report]
+
+                # Dimensions
+                output_dimensions = [
+                    # Date
+                    {"name": f"ga:{PERIOD}"},
+                    *subReport["Dims"],
+                ]
+
+                # Construct your Request
+                report_request = {
+                    "reportRequests": [
+                        {
+                            "dateRanges": [
+                                {
+                                    "startDate": view_details["startDate"],
+                                    "endDate": END_DATE,
+                                }
+                            ],
+                            "metrics": subReport["metrics"],
+                            "dimensions": output_dimensions,
+                        }
+                    ]
                 }
-            ]
-        }
 
-        report_name = f"./reports/{view_details['name']}/{PERIOD}_{REPORT_CODE}.csv"
+                report_name = f"./reports/{view_details['name']}/{PERIOD}_{report}_{sub_report}.csv"
 
-        try:
-            print(f"fetching data for '{view_details['name']} {REPORT_CODE}'")
-            ga_report = run_report(report_request, view_id, GA_API_KEYS)
-            ga_report.to_csv(report_name, index=True)
+                try:
+                    print(
+                        f"fetching data for '{view_details['name']} {report} {sub_report}'"
+                    )
+                    ga_report = run_report(report_request, view_id, GA_API_KEYS)
+                    ga_report.to_csv(report_name, index=True)
 
-            print(
-                f"'{view_details['name']} {REPORT_CODE}' Report saved successfully!! :tada:\n"
-            )
-        except Exception as exp:
-            print(f"Error occured: {exp}")
+                    print(
+                        f"'{view_details['name']} {report} {sub_report}' Report saved successfully!! :tada:\n"
+                    )
+                except Exception as exp:
+                    print(f"Error occured: {exp}")
